fe8cf610b593910cb63372f691aa2a183165324e
Kervintz Noel <10268341+kervcode@users.noreply.github.com>
2019-12-26 19:55:53 -0500

    create game class template

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,15 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ class Game {
+     constructor () {
+        this._missed = 0,
+        this._phrases = [], //need to add 5 phrases inside this array
+        this._activePhrase = null;
+     }
+
+     startGame(){
+         this._activePhrase = getRandomPhrase();
+     }
+ }
